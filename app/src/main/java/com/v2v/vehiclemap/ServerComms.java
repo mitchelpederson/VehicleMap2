28ad3c4628b5b335f00971911f6eb618edc680b5
Conrad H. Appel, IV <me@conradhappeliv.com>
2017-02-08 11:20:00 -0600

    HTTP GET added with JSON Array Parsing

--- app/src/main/java/com/v2v/vehiclemap/ServerComms.java
+++ app/src/main/java/com/v2v/vehiclemap/ServerComms.java
@@ -1,19 +1,33 @@
 package com.v2v.vehiclemap;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 // TODO: implement
-//public class ServerComms implements Communicator {
-//    @Override
-//    public ArrayList<Car> getAllCars() {
-//        // send GET req with payload { "given_id": "thiscarid" }
-//        // request returns [{given_id, lat, long, speed, bearing}]
-//        return null;
-//    }
-//
-//    @Override
-//    public void sendLocation(Car c) {
-//        // send POST req with payload { given_id, lat, long, speed, bearing } (everything optional)
-//        // request returns the current car's DB entry
-//    }
-//}
+public class ServerComms {
+    public static ArrayList<Car> getAllCars() throws JSONException {
+        // send GET req with payload { "given_id": "thiscarid" }
+        CarGetter dataGet = new CarGetter();
+        JSONArray jsonarray = dataGet.doInBackground(); // gets json array
+
+        for (int i = 0; i < jsonarray.length(); i++) {
+            JSONObject jsonobject = jsonarray.getJSONObject(i);
+            String given_id = jsonobject.getString("given_id");
+            String lat = jsonobject.getString("lat");
+            String lng = jsonobject.getString("long");
+            String speed = jsonobject.getString("speed");
+            String bearing = jsonobject.getString("bearing");
+        }
+
+        // request returns [{given_id -> String, lat, long, speed, bearing}]
+        return null;
+    }
+
+    public static void sendLocation(Car c) {
+        // send POST req with payload { given_id, lat, long, speed, bearing } (everything optional)
+        // request returns the current car's DB entry
+    }
+}
