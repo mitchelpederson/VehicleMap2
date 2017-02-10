1218ecee22790933000a602d9cbf4bd6fa043a8b
Mitchel Pederson <mpederson@smu.edu>
2017-02-10 14:55:03 -0600

    Get location from GPS now instead of hardcoded values           New branch

---
+++ app/src/main/java/com/v2v/vehiclemap/OverlayView.java
@@ -0,0 +1,25 @@
+package com.v2v.vehiclemap;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+/**
+ * Created by Mitchel on 2/1/2017.
+ */
+
+public class OverlayView extends GLSurfaceView {
+
+    GLSurfaceView.Renderer renderer;
+
+    public OverlayView(Context context) {
+        super(context);
+
+        //renderer = new ;
+
+    }
+
+
+    protected void onCreate() {
+
+     }
+}
